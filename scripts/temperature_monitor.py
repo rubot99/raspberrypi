64ab8afdaaee1124af69476a6619748971c17ef8
Reuben <helix99@gmail.com>
2020-10-06 19:28:21 +0100

    Added scripts

--- scripts/temperature_monitor.py
+++ scripts/temperature_monitor.py
@@ -9,7 +9,7 @@ x = []
 y = []
 
 def write_temp(temp):
-    with open("/home/pi/cpu_temp.csv", "a") as log:
+    with open("/home/pi/dataLogs/cpu_temp_monitor.csv", "a") as log:
         log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
 
 #def graph(temp):
